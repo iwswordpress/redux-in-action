e2783c7473e3b07ea1d645c15a5a8471788c8e9d
iwswordpress <iwswordpress@gmail.com>
2020-10-02 15:35:47 +0100

    adding EDIT

--- src/components/TasksPage.js
+++ src/components/TasksPage.js
@@ -7,7 +7,7 @@ class TasksPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showNewCardForm: true,
+      showNewCardForm: false,
       title: '',
       description: ''
     };
