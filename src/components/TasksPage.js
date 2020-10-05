ce99ebcbc233fdee2ea4b15deeef8dff10fc1197
iwswordpress <iwswordpress@gmail.com>
2020-10-05 12:56:54 +0100

    CH04

--- src/components/TasksPage.js
+++ src/components/TasksPage.js
@@ -1,101 +1,88 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import TaskList from './TaskList';
 
 import { TASK_STATUSES } from '../constants';
 
-class TasksPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showNewCardForm: false,
-      title: '',
-      description: '',
-    };
-  }
-
-  onTitleChange = e => {
-    this.setState({ title: e.target.value });
+const TasksPage = props => {
+  const [title, setTitle] = useEffect('');
+  const [description, setDescription] = useEffect('');
+  const [showNewCardForm, setShowNewCardForm] = useEffect(false);
+  const onTitleChange = e => {
+    setTitle({ title: e.target.value });
   };
 
-  onDescriptionChange = e => {
-    this.setState({ description: e.target.value });
+  const onDescriptionChange = e => {
+    setDescription({ description: e.target.value });
   };
 
-  resetForm() {
-    this.setState({
-      showNewCardForm: false,
-      title: '',
-      description: '',
-    });
-  }
+  const resetForm = () => {
+    setTitle('');
+    setDescription('');
+    setShowNewCardForm(false);
+  };
 
-  onCreateTask = e => {
+  const onCreateTask = e => {
     e.preventDefault();
-    this.props.onCreateTask({
-      title: this.state.title,
-      description: this.state.description,
+    props.onCreateTask({
+      title,
+      description
     });
-    this.resetForm();
+    resetForm();
   };
 
-  toggleForm = () => {
-    this.setState({ showNewCardForm: !this.state.showNewCardForm });
+  const toggleForm = () => {
+    setShowNewCardForm({ showNewCardForm: !showNewCardForm });
   };
 
-  render() {
-    if (this.props.isLoading) {
-      return (
-        <div className="tasks-loading">
-          Loading...
-        </div>
-      );
-    }
-
-    return (
-      <div className="tasks">
-        <div className="tasks-header">
-          <button className="button button-default" onClick={this.toggleForm}>
-            + New task
-          </button>
-        </div>
-        {this.state.showNewCardForm &&
-          <form className="new-task-form" onSubmit={this.onCreateTask}>
-            <input
-              className="full-width-input"
-              onChange={this.onTitleChange}
-              value={this.state.title}
-              type="text"
-              placeholder="title"
-            />
-            <input
-              className="full-width-input"
-              onChange={this.onDescriptionChange}
-              value={this.state.description}
-              type="text"
-              placeholder="description"
-            />
-            <button className="button" type="submit">
-              Save
-            </button>
-          </form>}
-        <div className="task-lists">
-          {TASK_STATUSES.map(status => {
-            const statusTasks = this.props.tasks.filter(
-              task => task.status === status
-            );
-            return (
-              <TaskList
-                key={status}
-                status={status}
-                tasks={statusTasks}
-                onStatusChange={this.props.onStatusChange}
-              />
-            );
-          })}
-        </div>
-      </div>
-    );
+  if (props.isLoading) {
+    return <div className='tasks-loading'>Loading...</div>;
   }
-}
+
+  return (
+    <div className='tasks'>
+      <div className='tasks-header'>
+        <button className='button button-default' onClick={toggleForm}>
+          + New task
+        </button>
+      </div>
+      {showNewCardForm && (
+        <form className='new-task-form' onSubmit={onCreateTask}>
+          <input
+            className='full-width-input'
+            onChange={onTitleChange}
+            value={title}
+            type='text'
+            placeholder='title'
+          />
+          <input
+            className='full-width-input'
+            onChange={onDescriptionChange}
+            value={description}
+            type='text'
+            placeholder='description'
+          />
+          <button className='button' type='submit'>
+            Save
+          </button>
+        </form>
+      )}
+      <div className='task-lists'>
+        {TASK_STATUSES.map(status => {
+          const statusTasks = props.tasks.filter(
+            task => task.status === status
+          );
+          return (
+            <TaskList
+              key={status}
+              status={status}
+              tasks={statusTasks}
+              onStatusChange={props.onStatusChange}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default TasksPage;
